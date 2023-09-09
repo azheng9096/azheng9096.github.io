292286e7385e49a8d34a741a0f2790989fd0d430
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-09-09 18:46:35 -0400

    navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import NavBar from "@/components/navbar";
+
+const navLinks = [
+  { title: "Games", href: "#games" },
+  { title: "Websites", href: "#websites" },
+  { title: "Programs", href: "programs" },
+];
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <NavBar links={navLinks} />
+    </main>
+  );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import "@/styles/components/navbar.scss";
+import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
+
+type Props = {
+  links: NavBarLink[];
+};
+
+type NavBarLink = {
+  title: string;
+  href: string;
+};
+
+export default function NavBar({ links }: Props) {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="navbar navbar-dark">
+      <Container fluid>
+        <Navbar.Brand href="#" className="mb-0 h1">
+          AZ
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            {links.map((link) => (
+              <Nav.Link href={link.href}>{link.title}</Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
